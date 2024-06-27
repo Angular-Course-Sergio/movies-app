0a8d0ed53bd911340c5cc5ac8d9fb07bb1e2084f
Sergio Barreras <sergiobg.isc@gmail.com>
2024-06-27 08:52:29 -0700

    lazy image component

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardComponent } from './components/card/card.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SearchBoxComponent, CardComponent],
-  imports: [],
+  imports: [SharedModule],
   exports: [],
 })
 export class MoviesModule {}
