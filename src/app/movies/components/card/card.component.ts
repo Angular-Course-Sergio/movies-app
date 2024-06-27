820644b9445d4b7218e3aecf01b4d8c38cc5bc5c
Sergio Barreras <sergiobg.isc@gmail.com>
2024-06-27 08:43:46 -0700

    card component and movie interface

---
+++ src/app/movies/components/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from '../../interfaces/movies.interface';
+
+@Component({
+  selector: 'movies-card',
+  templateUrl: './card.component.html',
+})
+export class CardComponent implements OnInit {
+  @Input() public movie!: Movie;
+
+  ngOnInit(): void {
+    if (!this.movie) throw new Error('Movie property is required');
+  }
+}
---
+++ src/app/movies/interfaces/movies.interface.ts
@@ -0,0 +1,16 @@
+export interface Movie {
+  adult: boolean;
+  backdropPath: string;
+  genreIds: string[];
+  id: number;
+  originalLanguage: string;
+  originalTitle: string;
+  overview: string;
+  popularity: number;
+  posterPath: string;
+  releaseDate: Date;
+  title: string;
+  video: boolean;
+  voteAverage: number;
+  voteCount: number;
+}
