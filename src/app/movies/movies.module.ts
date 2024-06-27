cc15ce048a0b89245555b15210606a7af7cbcfba
Sergio Barreras <sergiobg.isc@gmail.com>
2024-06-27 08:47:29 -0700

    card component html

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [SearchBoxComponent],
+  declarations: [SearchBoxComponent, CardComponent],
   imports: [],
   exports: [],
 })
